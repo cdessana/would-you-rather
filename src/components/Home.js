be65a6f70d5f6d4363248595792543c8ed963beb
Carmina Nascimento <cdessana@gmail.com>
2021-11-16 23:22:03 -0400

    feat: add tab component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,64 @@
 import React, {Component} from "react";
 import {connect} from 'react-redux'
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import TabPanel from './TabPanel'
 
-class Dashboard extends Component {
+class Home extends Component {
+
+    state = {
+        tabValue : 0
+    };
+    
     render()
     {
-        // console.log(this.props)
+        const handleChange = (event, newValue) => {
+            this.setState(() => ({
+                tabValue: newValue
+            }))
+        };
+
         return (
             <div>
                 <h3>The questions</h3>
-                <ul>
+
+                <Box sx={{ width: '50%' }}>
+                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                    <Tabs
+                    value={this.state.tabValue}
+                    onChange={handleChange}
+                    >
+                    <Tab label="Unanswered Questions" />
+                    <Tab label="Answered Questions" />
+                    </Tabs>
+                </Box>
+                <TabPanel value={this.state.tabValue} index={0}>
+                    <h3>Unanswered Questions</h3>
+                </TabPanel>
+                <TabPanel value={this.state.tabValue} index={1}>
+                <h3>Answered Questions</h3>
+                </TabPanel>
+                </Box>
+
+
+
+                {/* <ul>
                     {this.props.questionIds.map((id) => (
                         <li
                             key = {id}>
                             <div>Question id: {id}</div>
                         </li>
+
+                        
                     ))}
-                </ul>
+                </ul> */}
             </div>
         )
     }
 }
+
+
 
 function mapStateToProps({questions}){
     return {
@@ -27,4 +66,4 @@ function mapStateToProps({questions}){
     }
 }
 
-export default connect(mapStateToProps)(Dashboard)
+export default connect(mapStateToProps)(Home)
