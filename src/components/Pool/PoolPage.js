b1406edbe9fb76b764df6d0bfd8bfdb97bba1f96
Carmina Nascimento <cdessana@gmail.com>
2021-11-21 18:20:15 -0400

    feat: add pool result scratch

--- src/components/Pool/PoolPage.js
+++ src/components/Pool/PoolPage.js
@@ -1,20 +1,26 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import Pool from './Pool';
+import PoolResult from './PoolResult';
 
 class PoolPage extends Component {
 
     
 
     render() {
+
+        const {hasAnswered} = this.props
+
         console.log('props: ', this.props)
         const {id} = this.props
         console.log(id)
         
         return(
+
             <div>
-                POOL PAGE
-                <Pool id ={id}/>
+            {hasAnswered
+            ? <PoolResult id ={id}/>
+            : <Pool id ={id}/>}
             </div>
         )
     }
@@ -23,10 +29,29 @@ class PoolPage extends Component {
 function mapStateToProps({authedUser, questions, users}, props){
 
     const {id} = props.match.params
+    const user = users[authedUser]
+    var userAnswered = Object.keys(users[authedUser].answers);
+    var hasAnswered = userAnswered.includes(id)
+    console.log('mapStateToProps - hasAnswered', hasAnswered)
+
+    
+    const question = questions[id]
+    const questionAuthor = users[question.author]
+    const questionAuthorName = questionAuthor.name
+    const questionAuthorAvatar = questionAuthor.avatarURL
+    const optionOne = question.optionOne.text
+    const optionTwo = question.optionTwo.text
 
     return {
         id,
-        //answers: ?
+        authedUser,
+        hasAnswered,
+        // question : {
+        //     questionAuthorName,
+        //     questionAuthorAvatar,
+        //     optionOne,
+        //     optionTwo
+        // }
     }
 
 }
--- src/components/Pool/PoolResult.js
+++ src/components/Pool/PoolResult.js
@@ -1,44 +1,82 @@
 import { Component } from 'react';
 import { connect } from 'react-redux';
+import Box from '@mui/material/Box';
 
 class PollResult extends Component {
     render() {
-      return ()
+    
+    const {id, authedUser, question, userVote} = this.props
+      
+    return (
+          <div>
+              <Box className='box'>
+                  
+                  <div className="pool-sneak-js-top">
+                      Asked by {question.authorName}
+                  </div>
+          
+                  <div className="pool-sneak-js-bottom">
+                      
+                      <div className='profile-box'>
+                          <div className='profile-picture'> 
+                              <img src={question.authorAvatar} alt="user-profile" />
+                          </div>
+                      </div>
+          
+                      <div className="pool-sneak-js-box">   
+                          <h3>Results</h3>
+                          <div className='pool-result'>
+                            <h4>Would you rather {question.optionOneText}?</h4>
+                          </div>
+
+                          <div className='pool-result'>
+                            <h4>Would you rather {question.optionTwoText}?</h4>
+                          </div>
+                      </div>
+                  </div>
+              </Box>
+          </div>
+      )
     }
 }
 
 function mapStateToProps({ authedUser, users, questions }, props) {
 
+    const optionOne = 'optionOne'
+    const optionTwo = 'optionTwo'
 
+    console.log('POOL RESULT - mapStateToProps')
+    const id = props.id
+    // console.log('id: ', props.id)
 
+    const question = questions[id]
+    console.log(question)
+    const questionAuthor = users[question.author]
+    const questionAuthorName = questionAuthor.name
+    const questionAuthorAvatar = questionAuthor.avatarURL
 
-    
-    // const { id } = props.match.params;
-  
-    // const question = findMatchingQuestionId(id, questions);
-  
-    // const author = findMatchingUserId(question.author, users);
-  
-    // const votesForFirst = question.optionOne.votes.length;
-  
-    // const votesForSecond = question.optionTwo.votes.length;
-  
-    // const votes = votesForFirst + votesForSecond;
-  
-    // const firstOptionVoters = new Set(question.optionOne.votes);
-  
-    // const answer = firstOptionVoters.has(user.id) ? 'optionOne' : 'optionTwo';
-  
-    // return {
-    //   id,
-    //   author,
-    //   question,
-    //   user,
-    //   votesForFirst,
-    //   votesForSecond,
-    //   votes,
-    //   answer,
-    // };
+    const optionOneVotes = question[optionOne].votes.length
+    const optionTwoVotes = question[optionTwo].votes.length
+
+    const optionOneText = question[optionOne].text
+    const optionTwoText = question[optionTwo].text
+
+    const user = users[authedUser]
+    const userVote = user.answers[id]
+
+    return {
+        id,
+        authedUser,
+        question:{
+            authorName: questionAuthorName,
+            authorAvatar: questionAuthorAvatar,
+            optionOneVotes,
+            optionTwoVotes,
+            optionOneText,
+            optionTwoText,
+        },
+        userVote,
+    }
   }
 
 
