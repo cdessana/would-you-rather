318529c205748f3788d2dead1957dc3df1dee607
Carmina Nascimento <cdessana@gmail.com>
2021-11-21 08:46:30 -0400

    feat: add leaderboard

---
+++ src/components/Leaderboard/UserRank.js
@@ -0,0 +1,43 @@
+import { Component } from 'react';
+import { connect } from 'react-redux';
+import './Leaderboard.css';
+
+class UserRank extends Component {
+    
+
+    constructor(props) {
+        super(props);
+    
+        console.log('props constructor: ', this.props)
+    }
+
+
+    render(){
+
+        return(
+        <ul>
+            <li>
+                User: {this.props.userName}
+            </li>
+            <li>
+
+                Score: {this.props.points}
+            </li>
+            <li>
+
+                Answered Questions: {this.props.answers}
+            </li>
+            <li>
+
+                Created Questions: {this.props.questions}
+            </li>
+            
+            
+            
+        </ul>
+       
+        )
+    }
+}
+
+export default UserRank
